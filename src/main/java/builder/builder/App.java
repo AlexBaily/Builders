0e1e2c74812281ab11e7810306b684062a54e859
Alexander Baily <alex.d.baily@gmail.com>
2017-02-28 20:09:30 +0000

    Added character reference

--- src/main/java/builder/builder/App.java
+++ src/main/java/builder/builder/App.java
@@ -5,11 +5,11 @@ public class App {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		Boss angel = 
-				new Boss.Builder("Archimonde").atSize("Massive")
-				.withHp(20).withType(Type.DEMON).build();
+		Boss boss1 = 
+				new Boss.Builder("M.Bison").atSize("Medium")
+				.withHp(20).withType(Type.HUMAN).build();
 		
-		System.out.println(angel.toString());
+		System.out.println(boss1.toString());
  		
 	}
 
